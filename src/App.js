80166dcd79c0294a332218c8e5019b59d9bc4226
yurii-shendryk <yuriishendryk93@gmail.com>
2021-04-08 15:39:16 +0300

    Переробив класи на функціональні компоненти з хуками

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import shortid from 'shortid';
 import Container from './components/Container';
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 import Filter from './components/Filter';
-class App extends Component {
-  state = {
+const App = () => {
+  const [state, setState] = useState({
     contacts: [],
     filter: '',
-  };
+  });
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem('contacts'));
+    setState(prevState => ({
+      ...prevState,
+      contacts: data,
+    }));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(state.contacts));
+  }, [state.contacts]);
+
+  const { contacts, filter } = state;
+
   // Додаємо об'єкт
-  addContact = ({ name, number }) => {
-    const { contacts } = this.state;
+  const addContact = ({ name, number }) => {
     const contactsNames = contacts.map(contact => contact.name);
     if (contactsNames.includes(name)) {
       alert(`${name}is already in contacts`);
@@ -23,58 +37,56 @@ class App extends Component {
       number,
     };
 
-    this.setState(prevState => {
-      return { contacts: [contact, ...prevState.contacts] };
-    });
+    setState(prevState => ({
+      ...prevState,
+      contacts: [...prevState.contacts, contact],
+    }));
   };
+
   // Видаляємо об'єкт
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+  const deleteContact = contactId => {
+    setState(prevState => ({
+      ...prevState,
+      contacts: contacts.filter(contact => contact.id !== contactId),
     }));
   };
   // змінюємо інпут фільтрації
-  changeFilter = ({ currentTarget }) => {
-    this.setState({
+  const changeFilter = ({ currentTarget }) => {
+    setState({
+      ...state,
       filter: currentTarget.value,
     });
   };
   // Фільтруємо існуючі контакти
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
-
+  const getFilteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(normalizedFilter),
     );
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-
-    return (
-      <>
-        <Container>
-          <h1>Phonebook</h1>
-          {/* форма для введення контактних даних  */}
-          <ContactForm onSubmitForm={this.addContact} />
-          {/* якщо контакти є, то відображаємо список і фільтр */}
-          {contacts.length > 0 && (
-            <>
-              <h2>Contacts</h2>
-              {/* фільтрація контактів */}
-              <Filter filter={filter} onChangeFilter={this.changeFilter} />
-              {/* список контактів */}
-              <ContactList
-                contacts={this.getFilteredContacts()}
-                onDeleteContact={this.deleteContact}
-              />
-            </>
-          )}
-        </Container>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Container>
+        <h1>Phonebook</h1>
+        {/* форма для введення контактних даних  */}
+        <ContactForm onSubmitForm={addContact} />
+        {/* якщо контакти є, то відображаємо список і фільтр */}
+        {contacts.length > 0 && (
+          <>
+            <h2>Contacts</h2>
+            {/* фільтрація контактів */}
+            <Filter filter={filter} onChangeFilter={changeFilter} />
+            {/* список контактів */}
+            <ContactList
+              contacts={getFilteredContacts()}
+              onDeleteContact={deleteContact}
+            />
+          </>
+        )}
+      </Container>
+    </>
+  );
+};
 
 export default App;
